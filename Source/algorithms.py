5a7bdade18d4fe28dbae6429adeb5953cb5c75ba
Saman Khamesian <saman.khamesian@gmail.com>
2019-07-18 19:07:55 +0430

    Source code added.

---
+++ Source/algorithms.py
@@ -0,0 +1,27 @@
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.metrics.cluster import normalized_mutual_info_score as nmi
+from source import ectkmeans
+
+
+def run_ectkmeans(data, labels):
+    predicts = ectkmeans.fit(data)
+    _nmi = nmi(labels, predicts, average_method='geometric')
+
+    print('ECT-KMeans Algorithm Finished!')
+    return _nmi, predicts
+
+
+def run_kmeans(data, labels):
+    predicts = KMeans().fit(X=data).labels_
+    _nmi = nmi(labels, predicts, average_method='geometric')
+
+    print('KMeans Algorithm Finished!')
+    return _nmi
+
+
+def run_hierarchical(data, labels):
+    predicts = AgglomerativeClustering().fit(X=data).labels_
+    _nmi = nmi(labels, predicts, average_method='geometric')
+
+    print('Hierarchical Algorithm Finished!')
+    return _nmi
---
+++ Source/comparator.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as figure
+
+from source.algorithms import *
+from source.utility import load_dataset
+from source.config import COLOR, K
+
+
+def plot_dataset(data, labels):
+    for i in range(K):
+        figure.scatter(x=data[labels == i, 0], y=data[labels == i, 1], c=COLOR[i], s=20, edgecolors='k', label='Class ' + str(i))
+    figure.xlabel('X')
+    figure.ylabel('Y')
+    figure.title('Dataset Clustering Result')
+
+    figure.legend()
+    figure.show()
+
+
+def compare_clustering_algorithms(dataset_name, data, labels):
+
+    print('Running Algorithms ...')
+    nmi_kmeans = run_kmeans(data=data, labels=labels)
+    nmi_hierarchical = run_hierarchical(data=data, labels=labels)
+    nmi_ectkmeans, predicts = run_ectkmeans(data, labels)
+
+    if len(data[0]) == 2:
+        plot_dataset(data, predicts)
+
+    print('\nResults for ' + dataset_name + ' :')
+    print('NMI K-Means : ' + str(nmi_kmeans))
+    print('NMI Hierarchical : ' + str(nmi_hierarchical))
+    print('NMI ECT-KMeans : ' + str(nmi_ectkmeans))
+    print('\n--------------------------------------\n')
+
+
+def test_algorithms():
+    resource = load_dataset()
+    for dataset_name in resource:
+        data = resource[dataset_name]['data']
+        labels = resource[dataset_name]['labels']
+        compare_clustering_algorithms(dataset_name, data, labels)
+
+
+if __name__ == '__main__':
+    test_algorithms()
